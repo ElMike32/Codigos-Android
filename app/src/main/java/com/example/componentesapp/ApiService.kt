b44d447f009457633dcfb3aac246bcb09b0959ba
ElMike32 <miguelitojhz@gmail.com>
2026-08-28 11:27:24 -0600

    Update ApiService.kt

--- app/src/main/java/com/example/componentesapp/ApiService.kt
+++ app/src/main/java/com/example/componentesapp/ApiService.kt
@@ -1,5 +1,7 @@
 package com.example.componentesapp
 
+import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
@@ -11,10 +13,22 @@ interface ApiService {
 }
 
 object ApiClient {
+    // Configurar OkHttpClient para seguir redirecciones de Google Apps Script (HTTP 302 / 307)
+    private val okHttpClient = OkHttpClient.Builder()
+        .followRedirects(true)
+        .followSslRedirects(true)
+        .build()
+
+    // Configurar Gson en modo permisivo (lenient) por si la respuesta viene con caracteres o envoltorios
+    private val gson = GsonBuilder()
+        .setLenient()
+        .create()
+
     val instance: ApiService by lazy {
         Retrofit.Builder()
             .baseUrl("https://script.google.com/")
-            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create(gson))
             .build()
             .create(ApiService::class.java)
     }
