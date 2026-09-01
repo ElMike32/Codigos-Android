d8c922628743ef92e6f6e64885be1a77c73df053
ElMike32 <miguelitojhz@gmail.com>
2026-09-01 15:12:05 -0600

    Update ApiService.kt

--- app/src/main/java/com/example/componentesapp/ApiService.kt
+++ app/src/main/java/com/example/componentesapp/ApiService.kt
@@ -5,23 +5,26 @@ import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
+import retrofit2.http.Query
 import retrofit2.http.Url
 import java.util.concurrent.TimeUnit
 
 interface ApiService {
     @GET
-    suspend fun getDataFromScript(@Url url: String): ApiResponse
+    suspend fun getDataFromScript(
+        @Url url: String,
+        @Query("deviceId") deviceId: String
+    ): ApiResponse
 }
 
 object ApiClient {
-    // Configurar OkHttpClient con 60 segundos de Timeout para evitar el error de conexión
     private val okHttpClient = OkHttpClient.Builder()
         .connectTimeout(60, TimeUnit.SECONDS)
         .readTimeout(60, TimeUnit.SECONDS)
         .writeTimeout(60, TimeUnit.SECONDS)
         .followRedirects(true)
         .followSslRedirects(true)
-        .retryOnConnectionFailure(true) // Reintentar automáticamente en fallos temporales de red
+        .retryOnConnectionFailure(true)
         .build()
 
     private val gson = GsonBuilder()
